1b9863affb6d29f02f04b316dc0c900e6e046b13
Mitchellbrine <awsomeman500@gmail.com>
2014-07-19 17:35:29 -0700

    Abra-cadabra sheep-cadabra! (A wild sheep appeared!)

--- src/main/java/magiciansartifice/items/wand/Wand.java
+++ src/main/java/magiciansartifice/items/wand/Wand.java
@@ -1,9 +1,14 @@
 package magiciansartifice.items.wand;
 
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import magiciansartifice.MagiciansArtifice;
 import magiciansartifice.ModInfo;
+import magiciansartifice.items.ItemRegistry;
+import net.minecraft.entity.passive.EntitySheep;
 import net.minecraft.item.Item;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.ServerChatEvent;
 
 public class Wand extends Item {
 
@@ -13,6 +18,18 @@ public class Wand extends Item {
         this.setUnlocalizedName("magiciansWand");
         this.setTextureName(ModInfo.MODID + ":magicianWand");
         GameRegistry.registerItem(this,this.getUnlocalizedName().substring(5));
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void magicWords(ServerChatEvent event) {
+        if (event.player.getCurrentEquippedItem() != null && event.player.getCurrentEquippedItem().getItem() == ItemRegistry.wand) {
+            if (event.message.contains("Abra-cadabra") || event.message.contains("Sheep-cadabra")) {
+                EntitySheep sheep = new EntitySheep(event.player.worldObj);
+                sheep.setLocationAndAngles(event.player.posX,event.player.posY + 1,event.player.posZ,event.player.cameraYaw,event.player.cameraPitch);
+                event.player.worldObj.spawnEntityInWorld(sheep);
+            }
+        }
     }
 
 }
