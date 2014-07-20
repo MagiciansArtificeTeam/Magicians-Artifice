ebec8bd346889786531fcdced7086bd1c5cb681f
Mitchellbrine <awsomeman500@gmail.com>
2014-07-19 17:57:10 -0700

    Added more to the test spell

--- src/main/java/magiciansartifice/items/wand/Wand.java
+++ src/main/java/magiciansartifice/items/wand/Wand.java
@@ -5,10 +5,15 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import magiciansartifice.MagiciansArtifice;
 import magiciansartifice.ModInfo;
 import magiciansartifice.items.ItemRegistry;
+import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.passive.EntitySheep;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.ServerChatEvent;
+
+import java.util.Random;
 
 public class Wand extends Item {
 
@@ -24,10 +29,27 @@ public class Wand extends Item {
     @SubscribeEvent
     public void magicWords(ServerChatEvent event) {
         if (event.player.getCurrentEquippedItem() != null && event.player.getCurrentEquippedItem().getItem() == ItemRegistry.wand) {
-            if (event.message.contains("Abra-cadabra") || event.message.contains("Sheep-cadabra")) {
-                EntitySheep sheep = new EntitySheep(event.player.worldObj);
-                sheep.setLocationAndAngles(event.player.posX,event.player.posY + 1,event.player.posZ,event.player.cameraYaw,event.player.cameraPitch);
-                event.player.worldObj.spawnEntityInWorld(sheep);
+            if (event.message.contains("Abra-cadabra")) {
+                if (event.message.contains("sheep-cadabra")) {
+                    if (!event.message.contains("egg")) {
+                        EntitySheep sheep = new EntitySheep(event.player.worldObj);
+                        Random random = new Random();
+                        sheep.setFleeceColor(random.nextInt(15));
+                        sheep.setLocationAndAngles(event.player.posX, event.player.posY + 1, event.player.posZ, event.player.cameraYaw, event.player.cameraPitch);
+                        event.player.worldObj.spawnEntityInWorld(sheep);
+                    } else {
+                        event.player.inventory.addItemStackToInventory(new ItemStack(Items.spawn_egg,1,91));
+                    }
+                }
+                if (event.message.contains("zombie-cadabra")) {
+                    if (!event.message.contains("egg")) {
+                        EntityZombie zombie = new EntityZombie(event.player.worldObj);
+                        zombie.setLocationAndAngles(event.player.posX, event.player.posY + 1, event.player.posZ, event.player.cameraYaw, event.player.cameraPitch);
+                        event.player.worldObj.spawnEntityInWorld(zombie);
+                    } else {
+                        event.player.inventory.addItemStackToInventory(new ItemStack(Items.spawn_egg,1,54));
+                    }
+                }
             }
         }
     }
