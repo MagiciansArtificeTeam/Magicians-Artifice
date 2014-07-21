bfba898abc23364321a2fe46d38d6c7d9081b343
Mitchellbrine <awsomeman500@gmail.com>
2014-07-20 19:58:02 -0700

    Added Book names and lore

--- src/main/java/magiciansartifice/items/ItemMagicBook.java
+++ src/main/java/magiciansartifice/items/ItemMagicBook.java
@@ -6,10 +6,12 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
+import java.util.List;
+
 public class ItemMagicBook extends Item {
 
     public ItemMagicBook() {
-        this.setUnlocalizedName("book");
+        this.setUnlocalizedName("magiciansartifice.book");
         this.setTextureName("magiciansartifice:book");
         this.setCreativeTab(MagiciansArtifice.tab);
         ItemRegistry.items.add(this);
@@ -22,5 +24,10 @@ public class ItemMagicBook extends Item {
         }
 
         return stack;
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, EntityPlayer player, List lore, boolean par4) {
+        lore.add("by Merlin");
     }
 }
