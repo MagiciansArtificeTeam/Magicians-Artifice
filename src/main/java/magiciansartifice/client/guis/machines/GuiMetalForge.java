a2185f4754ae3b105b396623df5879dac93aa66d
James Hollowell <jamesthollowell@gmail.com>
2014-07-21 21:15:29 -0400

    Add basic classes [just copied so far ;)]; changed the gui image (sorr poppy)

---
+++ src/main/java/magiciansartifice/client/guis/machines/GuiMetalForge.java
@@ -0,0 +1,53 @@
+package magiciansartifice.client.guis.machines;
+
+import magiciansartifice.containers.ContainerMysticAnvil;
+import magiciansartifice.libs.ModInfo;
+import magiciansartifice.tileentities.machines.TileEntityMysticAnvil;
+import magiciansartifice.utils.TextHelper;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
+
+/**
+ * Created by Millsy on 19/07/14.
+ */
+public class GuiMetalForge extends GuiContainer
+{
+
+    public static final ResourceLocation gui = new ResourceLocation(ModInfo.MODID, "textures/guis/mysticAnvilGUI.png");
+    private ContainerMetalForge container;
+    private TileEntityMysticAnvil te;
+
+    public GuiMetalForge(EntityPlayer player, TileEntityMysticAnvil tile)
+    {
+        super(new ContainerMysticAnvil(player, tile));
+        this.container = (ContainerMysticAnvil) this.inventorySlots;
+        this.te = tile;
+    }
+    
+    @Override
+    protected void drawGuiContainerForegroundLayer(int x, int y)
+    {
+        fontRendererObj.drawString(TextHelper.localize("gui.mystic.anvil"), xSize / 2 - fontRendererObj.getStringWidth(TextHelper.localize("gui.mystic.anvil")) / 2, 2, 0xffffff);
+        fontRendererObj.drawString(TextHelper.localize("container.inventory"), 8, ySize - 96 + 4, 0xffffff);
+    }
+    
+    @Override
+    protected void drawGuiContainerBackgroundLayer(float var1, int var2, int var3)
+    {
+        GL11.glColor4f(1F, 1F, 1F, 1F);
+        
+        Minecraft.getMinecraft().getTextureManager().bindTexture(gui);
+        drawTexturedModalRect(guiLeft, guiTop, 0, 0, xSize, ySize);
+        
+        int xStart = (width - xSize) / 2;
+        int yStart = (height - ySize) / 2;
+        this.drawTexturedModalRect(xStart, yStart, 0, 0, xSize, ySize);
+        
+        int i1 = this.te.getScaledProgress(24);
+        this.drawTexturedModalRect(xStart + 96, yStart + 35, 176, 14, i1 + 1, 16);
+        
+    }
+}
---
+++ src/main/java/magiciansartifice/containers/slot/SlotOutput.java
@@ -0,0 +1,40 @@
+/*
+ * ChowTime - Dynamically updating food mod for Minecraft
+ *     Copyright (C) 2014  Team JamCraft
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package magiciansartifice.containers.slot;
+
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+
+/**
+ * Created by James Hollowell on 5/17/2014.
+ */
+public class SlotOutput extends Slot
+{
+    public SlotOutput(IInventory par1IInventory, int par2, int par3, int par4)
+    {
+        super(par1IInventory, par2, par3, par4);
+    }
+
+    @Override
+    public boolean isItemValid(ItemStack par1ItemStack)
+    {
+        return false;
+    }
+}
