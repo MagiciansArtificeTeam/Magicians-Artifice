d7638862ec75c8e0e0d60b398a5d5b2b1d48142a
Mitchellbrine <awsomeman500@gmail.com>
2014-08-08 11:15:13 -0700

    Fixed the ritual for Poppy (don't know how to make it)

--- src/main/java/magiciansartifice/spells/rituals/RitualTeleport.java
+++ src/main/java/magiciansartifice/spells/rituals/RitualTeleport.java
@@ -2,24 +2,29 @@ package magiciansartifice.spells.rituals;
 
 import magiciansartifice.api.BasicRitual;
 import magiciansartifice.blocks.BlockRegistry;
+import magiciansartifice.blocks.magicblocks.BlockTeleportReciever;
 import magiciansartifice.tileentities.magic.TileEntityTeleportReciever;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.world.World;
 
 /**
  * Created by poppypoppop on 8/08/2014.
  */
 public class RitualTeleport extends BasicRitual {
-    public void startRitual(int x, int y, int z, World world, EntityPlayer player) { super.startRitual(x, y, z, world, player); }
+    public void startRitual(int x, int y, int z, World world, EntityPlayer player) { super.startRitual(x, y, z, world, player);
+    System.err.println("Started ritual");
+    }
 
     public boolean areAllBlocksCorrect(int x, int y, int z, World world, EntityPlayer player) {
         int x1 = x;
         int y1 = y;
         int z1 = z;
         z -= 2;
+
         for (int i = 0; i < 5; i++) {
             if (world.getBlock(x - 2, y, z + i) == BlockRegistry.storage) {
                 if (!(world.getBlockMetadata(x - 2, y, z + i) == 0)) {
@@ -35,7 +40,7 @@ public class RitualTeleport extends BasicRitual {
                 }
             }
         }
-
+        z += 0;
         x -= 2;
         for (int i = 0; i < 5; i++) {
             if (world.getBlock(x + i, y, z) == BlockRegistry.storage) {
@@ -61,7 +66,7 @@ public class RitualTeleport extends BasicRitual {
         }
 
         for (int i = 0; i < 3; i++) {
-            if (!(Item.getItemFromBlock(world.getBlock(x + i, y, z + 3)) == new ItemStack(Blocks.stonebrick, 1, 3).getItem() || world.getBlock(x + i, y, z + 2) == BlockRegistry.ritualCornerStone)) {
+            if (!(Item.getItemFromBlock(world.getBlock(x + i, y, z + 3)) == new ItemStack(Blocks.stonebrick, 1, 3).getItem() && world.getBlock(x + i, y, z + 2) != BlockRegistry.ritualCornerStone)) {
                 return false;
             }
         }
@@ -71,11 +76,32 @@ public class RitualTeleport extends BasicRitual {
                 return false;
             }
         }
+        System.err.println("Everything is awesome!");
         return true;
     }
 
     public void initEffect(int x, int y, int z, World world, EntityPlayer player) {
-        //TileEntityTeleportReciever te = (TileEntityTeleportReciever) world.getTileEntity(x, y, z);
-        //player.setPosition((double)te.getX(), (double)te.getY() + 1, (double) te.getZ());
+
+        System.err.println("Starting effect!");
+
+        for (int x1 = x - 50;x1 < x + 51;x1++) {
+            for (int z1 = z - 50;z1 < z + 51;z1++) {
+                if (world.getBlock(x1,y,z1) != null && world.getBlock(x1,y,z1) instanceof BlockTeleportReciever) {
+                    TileEntityTeleportReciever te = (TileEntityTeleportReciever) world.getTileEntity(x1, y, z1);
+                    if (te != null) {
+                        player.setLocationAndAngles((double) te.getX(), (double) te.getY() + 1, (double) te.getZ(),player.cameraPitch,player.cameraYaw);
+                        System.err.println("Finished effect!");
+                        return;
+                    }
+                }
+            }
+        }
+
+        if (player.getPlayerCoordinates().posX == x && player.getPlayerCoordinates().posY == y && player.getPlayerCoordinates().posZ == z) {
+            player.addChatComponentMessage(new ChatComponentTranslation("ritual.teleport.failure"));
+        } else {
+            player.addChatComponentMessage(new ChatComponentTranslation("ritual.teleport.success"));
+        }
+
     }
 }
