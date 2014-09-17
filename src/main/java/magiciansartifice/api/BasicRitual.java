4a2e7088c114ecbbb6c1bc9776487c0b2fc6f59a
James Mills <ThePugPug89@gmail.com>
2014-09-18 08:41:30 +1000

    Not much to update

--- src/main/java/magiciansartifice/api/BasicRitual.java
+++ src/main/java/magiciansartifice/api/BasicRitual.java
@@ -13,7 +13,6 @@ import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 
-@SuppressWarnings("null")
 public abstract class BasicRitual {
 
     private String unlocalizedName;
