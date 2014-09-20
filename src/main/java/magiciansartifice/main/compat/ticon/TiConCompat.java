103e68ba329241e33e955ceb515d6c20e7938696
James Mills <ThePugPug89@gmail.com>
2014-09-20 16:11:39 +1000

    added ticon smeltery compat

--- src/main/java/magiciansartifice/main/compat/ticon/TiConCompat.java
+++ src/main/java/magiciansartifice/main/compat/ticon/TiConCompat.java
@@ -1,15 +1,20 @@
 package magiciansartifice.main.compat.ticon;
 
+import magiciansartifice.main.blocks.BlockRegistry;
+import magiciansartifice.main.fluids.LiquidRegistry;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.FluidStack;
+import tconstruct.library.crafting.Smeltery;
+import tconstruct.smeltery.TinkerSmeltery;
 import cpw.mods.fml.common.Optional;
 
 /**
  * Created by Mitchellbrine on 2014.
  */
 public class TiConCompat {
-
     @Optional.Method(modid = "TConstruct")
     public static void registerStuff() {
-
+    	Smeltery.addMelting(new ItemStack(BlockRegistry.oresOres), 800, new FluidStack(LiquidRegistry.moltenStarSteel, 2000));
+    	Smeltery.addAlloyMixing(new FluidStack(LiquidRegistry.pureMoltenStarSteel, 2000), new FluidStack(TinkerSmeltery.moltenSteelFluid, 1000), new FluidStack(LiquidRegistry.moltenStarSteel, 1000));
     }
-
 }
--- src/main/java/magiciansartifice/main/fluids/LiquidRegistry.java
+++ src/main/java/magiciansartifice/main/fluids/LiquidRegistry.java
@@ -1,10 +1,12 @@
 package magiciansartifice.main.fluids;
 
-import cpw.mods.fml.common.registry.GameRegistry;
+import magiciansartifice.main.compat.ticon.fluids.BlockFluidPureStarSteel;
+import magiciansartifice.main.compat.ticon.fluids.BlockFluidStarSteel;
 import magiciansartifice.main.fluids.fluidblocks.BlockFluidMagicWater;
 import net.minecraft.block.Block;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 /**
  * Created by poppypoppop on 29/07/2014.
@@ -12,6 +14,10 @@ import net.minecraftforge.fluids.FluidRegistry;
 public class LiquidRegistry {
     public static Fluid magicWater;
     public static Block magicWaterBlock;
+    public static Fluid moltenStarSteel;
+    public static Block moltenStarSteelBlock;
+    public static Fluid pureMoltenStarSteel;
+    public static Block pureMoltenStarSteelBlock;
 
     public static void registerFluids() {
         magicWater = new Fluid("magicWater");
@@ -19,5 +25,17 @@ public class LiquidRegistry {
         magicWaterBlock = new BlockFluidMagicWater(magicWater);
         GameRegistry.registerBlock(magicWaterBlock, magicWaterBlock.getUnlocalizedName());
         magicWater.setUnlocalizedName(magicWaterBlock.getUnlocalizedName());
+        
+        moltenStarSteel = new Fluid("moltenStarSteel");
+        FluidRegistry.registerFluid(moltenStarSteel);
+        moltenStarSteelBlock = new BlockFluidStarSteel(moltenStarSteel);
+        GameRegistry.registerBlock(moltenStarSteelBlock, moltenStarSteelBlock.getUnlocalizedName());
+        moltenStarSteel.setUnlocalizedName(moltenStarSteelBlock.getUnlocalizedName());
+        
+        pureMoltenStarSteel = new Fluid("pureMoltenStarSteel");
+        FluidRegistry.registerFluid(pureMoltenStarSteel);
+        pureMoltenStarSteelBlock = new BlockFluidPureStarSteel(pureMoltenStarSteel);
+        GameRegistry.registerBlock(pureMoltenStarSteelBlock, pureMoltenStarSteelBlock.getUnlocalizedName());
+        pureMoltenStarSteel.setUnlocalizedName(pureMoltenStarSteelBlock.getUnlocalizedName());
     }
 }
