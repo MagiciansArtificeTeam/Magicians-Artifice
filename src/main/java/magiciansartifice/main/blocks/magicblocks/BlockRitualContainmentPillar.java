930418a86d087257c026917d12db4bc92135a2af
James Mills <ThePugPug89@gmail.com>
2014-10-01 12:36:53 +1000

    Texture changed so you can tell the things from stuff

--- src/main/java/magiciansartifice/main/blocks/magicblocks/BlockRitualContainmentPillar.java
+++ src/main/java/magiciansartifice/main/blocks/magicblocks/BlockRitualContainmentPillar.java
@@ -9,11 +9,9 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.util.IIcon;
-import net.minecraft.world.IBlockAccess;
 
 public class BlockRitualContainmentPillar extends Block {
-	public IIcon[] icons = new IIcon[18];
-	public boolean topOpen, bottomOpen;
+	public IIcon[] icons = new IIcon[16];
 	
 	public BlockRitualContainmentPillar() {
 		super(Material.rock);
@@ -26,89 +24,25 @@ public class BlockRitualContainmentPillar extends Block {
 	
 	@SideOnly(Side.CLIENT)
 	public void registerBlockIcons(IIconRegister ir) {
-		blockIcon = ir.registerIcon(ModInfo.MODID + ":containment/pillar/ritualContainmentPillarBottom");
 		icons[0] = ir.registerIcon(ModInfo.MODID + ":containment/pillar/ritualContainmentPillarBottom");
 		icons[1] = ir.registerIcon(ModInfo.MODID + ":containment/pillar/ritualContainmentPillarBottom");
 		icons[2] = ir.registerIcon(ModInfo.MODID + ":containment/pillar/ritualContainmentPillarSide1_0");
-		icons[3] = ir.registerIcon(ModInfo.MODID + ":containment/pillar/ritualContainmentPillarSide2_0");
+		icons[3] = ir.registerIcon(ModInfo.MODID + ":containment/pillar/ritualContainmentPillarSide1_0");
 		icons[4] = ir.registerIcon(ModInfo.MODID + ":containment/pillar/ritualContainmentPillarSide3_0");
 		icons[5] = ir.registerIcon(ModInfo.MODID + ":containment/pillar/ritualContainmentPillarSide4_0");
-		icons[6] = ir.registerIcon(ModInfo.MODID + ":containment/pillar/ritualContainmentPillarSide1_1");
-		icons[7] = ir.registerIcon(ModInfo.MODID + ":containment/pillar/ritualContainmentPillarSide2_1");
-		icons[8] = ir.registerIcon(ModInfo.MODID + ":containment/pillar/ritualContainmentPillarSide3_1");
-		icons[9] = ir.registerIcon(ModInfo.MODID + ":containment/pillar/ritualContainmentPillarSide4_1");
-		icons[10] = ir.registerIcon(ModInfo.MODID + ":containment/pillar/ritualContainmentPillarSide1_2");
-		icons[11] = ir.registerIcon(ModInfo.MODID + ":containment/pillar/ritualContainmentPillarSide2_2");
-		icons[12] = ir.registerIcon(ModInfo.MODID + ":containment/pillar/ritualContainmentPillarSide3_2");
-		icons[13] = ir.registerIcon(ModInfo.MODID + ":containment/pillar/ritualContainmentPillarSide4_2");
-		icons[14] = ir.registerIcon(ModInfo.MODID + ":containment/pillar/ritualContainmentPillarSide1_3");
-		icons[15] = ir.registerIcon(ModInfo.MODID + ":containment/pillar/ritualContainmentPillarSide2_3");
-		icons[16] = ir.registerIcon(ModInfo.MODID + ":containment/pillar/ritualContainmentPillarSide3_3");
-		icons[17] = ir.registerIcon(ModInfo.MODID + ":containment/pillar/ritualContainmentPillarSide4_3");
 	}
 	
 	@Override
 	@SideOnly(Side.CLIENT)
-    public IIcon getIcon(IBlockAccess blockAccess, int x, int y, int z, int side) {
-		switch(side) {
-    	case 0: return icons[0];
-    	case 1: return icons[1];
-    	case 2: return icons[2];
-    	case 3: return icons[3];
-    	case 4: return icons[4];
-    	case 5: return icons[5];
-    	}
-		return blockIcon;
-		//checkSides(blockAccess, x, y, z, side);
-        //return displaySides(blockAccess, x, y, z, side);
+    public IIcon getIcon(int side, int meta) {
+        switch(side) {
+        case 0: return icons[0];
+        case 1: return icons[1];
+        case 2: return icons[2];
+        case 3: return icons[3];
+        case 4: return icons[4];
+        case 5: return icons[5];
+        }
+        return blockIcon;
     }
-	
-	private void checkSides(IBlockAccess blockAccess, int x, int y, int z, int side) {
-		if (blockAccess.getBlock(x, y + 1, z) == this || blockAccess.getBlock(x, y + 1, z) == BlockRegistry.containmentTop ) { bottomOpen = true; }
-		if (blockAccess.getBlock(x, y - 1, z) == this) { topOpen = true; }
-	}
-	
-	private IIcon displaySides(IBlockAccess blockAccess, int x, int y, int z, int side) {
-		if (!topOpen && !bottomOpen) {
-			switch(side) {
-	    	case 0: return icons[0];
-	    	case 1: return icons[1];
-	    	case 2: return icons[2];
-	    	case 3: return icons[3];
-	    	case 4: return icons[4];
-	    	case 5: return icons[5];
-	    	}
-		}
-		if (!topOpen && bottomOpen) {
-			switch(side) {
-	    	case 0: return icons[0];
-	    	case 1: return icons[1];
-	    	case 2: return icons[6];
-	    	case 3: return icons[7];
-	    	case 4: return icons[8];
-	    	case 5: return icons[9];
-	    	}
-		}
-		if (topOpen && !bottomOpen) {
-			switch(side) {
-	    	case 0: return icons[0];
-	    	case 1: return icons[1];
-	    	case 2: return icons[10];
-	    	case 3: return icons[11];
-	    	case 4: return icons[12];
-	    	case 5: return icons[13];
-	    	}
-		}
-		if (topOpen && bottomOpen) {
-			switch(side) {
-	    	case 0: return icons[0];
-	    	case 1: return icons[1];
-	    	case 2: return icons[14];
-	    	case 3: return icons[15];
-	    	case 4: return icons[16];
-	    	case 5: return icons[17];
-	    	}
-		}
-		return blockIcon;
-	}
 }
--- src/main/java/magiciansartifice/main/blocks/magicblocks/BlockRitualCornerstone.java
+++ src/main/java/magiciansartifice/main/blocks/magicblocks/BlockRitualCornerstone.java
@@ -25,7 +25,7 @@ public class BlockRitualCornerstone extends BlockContainer {
     public BlockRitualCornerstone() {
         super(Material.iron);
         this.setBlockName("ritual.cornerStone");
-        this.setBlockTextureName(ModInfo.MODID + ":magicalblocks/ritualCornerstone");
+        this.setBlockTextureName(ModInfo.MODID + ":containment/pillar/ritualCornerstone");
         this.setBlockUnbreakable();
         this.setResistance(20000.0F);
         this.setCreativeTab(MagiciansArtifice.tab);
