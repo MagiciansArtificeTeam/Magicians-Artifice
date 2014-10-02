e4bd7bac4bae015819e62669a9dba35c57e0b825
James Mills <ThePugPug89@gmail.com>
2014-10-02 13:53:07 +1000

    Oops missed something

--- src/main/java/magiciansartifice/main/core/client/CustomItemRenderer.java
+++ src/main/java/magiciansartifice/main/core/client/CustomItemRenderer.java
@@ -56,6 +56,9 @@ public class CustomItemRenderer implements IItemRenderer {
                 if (item.getItem() == Item.getItemFromBlock(BlockRegistry.wandCarver)) {
                     renderWandCarver(0.5F, 15F, -0.5F, 0.09F);
                 }
+                if (item.getItem() == Item.getItemFromBlock(BlockRegistry.tank)) {
+                    renderTank(0.5F, 15F, -0.5F, 0.2F);
+                }
                 if (item.getItem() == ItemRegistry.magiciansWand) {
                     renderWand(0F, 0F, 0F, 0.1F, 25, 0, 0, 1);
                 }
@@ -63,13 +66,10 @@ public class CustomItemRenderer implements IItemRenderer {
                     renderWand(0F, 0F, 0F, 0.1F, 25, 0, 0, 2);
                 }
                 if (item.getItem() == ItemRegistry.magiciansWand3) {
-                	renderWand(0F, 0F, 0F, 0.1F, 25, 0, 0, 3);
+                    renderWand(0F, 0F, 0F, 0.1F, 25, 0, 0, 3);
                 }
                 if (item.getItem() == ItemRegistry.creativeWand) {
                     renderWand(0F, 0F, 0F, 0.1F, 25, 0, 0, 3);
-                }
-                if (item.getItem() == Item.getItemFromBlock(BlockRegistry.tank)) {
-                    renderTank(0.5F, 15F, -0.5F, 0.2F);
                 }
                 if (item.getItem() == ItemRegistry.beastClaw) {
                 	renderClaw(0F, 0.3F, 0F, 0.15F, 25, 0, 0, 0);
@@ -83,6 +83,9 @@ public class CustomItemRenderer implements IItemRenderer {
                 if (item.getItem() == Item.getItemFromBlock(BlockRegistry.wandCarver)) {
                     renderWandCarver(2F, 15F, 5F, 0.10F);
                 }
+                if (item.getItem() == Item.getItemFromBlock(BlockRegistry.tank)) {
+                    renderTank(2F, 15F, 5F, 0.10F);
+                }
                 if (item.getItem() == ItemRegistry.magiciansWand) {
                     renderWand(4F, 2F, 4F, 0.2F, 35, -45, -50, 1);
                 }
@@ -90,13 +93,10 @@ public class CustomItemRenderer implements IItemRenderer {
                     renderWand(4F, 2F, 4F, 0.2F, 35, -45, -50, 2);
                 }
                 if (item.getItem() == ItemRegistry.magiciansWand3) {
-                	renderWand(4F, 2F, 4F, 0.2F, 35, -45, -50, 3);
+                    renderWand(4F, 2F, 4F, 0.2F, 35, -45, -50, 3);
                 }
                 if (item.getItem() == ItemRegistry.creativeWand) {
                     renderWand(4F, 2F, 4F, 0.2F, 35, -45, -50, 3);
-                }
-                if (item.getItem() == Item.getItemFromBlock(BlockRegistry.tank)) {
-                    renderTank(2F, 15F, 5F, 0.10F);
                 }
                 if (item.getItem() == ItemRegistry.beastClaw) {
                     renderClaw(4.5F, -0.5F, 4.5F, 0.2F, 125, 260, 1, 10);
@@ -110,6 +110,9 @@ public class CustomItemRenderer implements IItemRenderer {
                 if (item.getItem() == Item.getItemFromBlock(BlockRegistry.wandCarver)) {
                     renderWandCarver(1F, 19F, 7F, 0.08F);
                 }
+                if (item.getItem() == Item.getItemFromBlock(BlockRegistry.tank)) {
+                    renderTank(1F, 19F, 7F, 0.08F);
+                }
                 if (item.getItem() == ItemRegistry.magiciansWand) {
                     renderWand(0F, 0F, 2.5F, 0.2F, 25, 0, 0, 1);
                 }
@@ -117,13 +120,10 @@ public class CustomItemRenderer implements IItemRenderer {
                     renderWand(0F, 0F, 2.5F, 0.2F, 25, 0, 0, 2);
                 }
                 if (item.getItem() == ItemRegistry.magiciansWand3) {
-                	renderWand(0F, 0F, 2.5F, 0.2F, 25, 0, 0, 3);
+                    renderWand(0F, 0F, 2.5F, 0.2F, 25, 0, 0, 3);
                 }
                 if (item.getItem() == ItemRegistry.creativeWand) {
                     renderWand(0F, 0F, 2.5F, 0.2F, 25, 0, 0, 3);
-                }
-                if (item.getItem() == Item.getItemFromBlock(BlockRegistry.tank)) {
-                    renderTank(1F, 19F, 7F, 0.08F);
                 }
                 if (item.getItem() == ItemRegistry.beastClaw) {
                 	renderClaw(0F, 5F, 0F, 0.2F, 135, 0, 45, 0);
@@ -137,20 +137,20 @@ public class CustomItemRenderer implements IItemRenderer {
                 if (item.getItem() == Item.getItemFromBlock(BlockRegistry.wandCarver)) {
                     renderWandCarver(-0.01F, 10F, 0.0F, 0.1F);
                 }
+                if (item.getItem() == Item.getItemFromBlock(BlockRegistry.tank)) {
+                    renderTank(-0.01F, 10F, 0.0F, 0.1F);
+                }
                 if (item.getItem() == ItemRegistry.magiciansWand) {
                     renderWand(-2.0F, -4.25F, 1F, 0.225F, 75, -10, 20, 1);
                 }
                 if (item.getItem() == ItemRegistry.magiciansWand2) {
-                	renderWand(-2.0F, -4.25F, 1F, 0.225F, 75, -10, 20, 2);
+                    renderWand(-2.0F, -4.25F, 1F, 0.225F, 75, -10, 20, 2);
                 }
                 if (item.getItem() == ItemRegistry.magiciansWand3) {
-                	renderWand(-2.0F, -4.25F, 1F, 0.225F, 75, -10, 20, 3);
+                    renderWand(-2.0F, -4.25F, 1F, 0.225F, 75, -10, 20, 3);
                 }
                 if (item.getItem() == ItemRegistry.creativeWand) {
                     renderWand(-2.0F, -4.25F, 1F, 0.225F, 75, -10, 20, 3);
-                }
-                if (item.getItem() == Item.getItemFromBlock(BlockRegistry.tank)) {
-                    renderTank(-0.01F, 10F, 0.0F, 0.1F);
                 }
                 if (item.getItem() == ItemRegistry.beastClaw) {
                 	renderClaw(0F, 0F, 0F, 0.225F, 0, 0, 0, 0);
