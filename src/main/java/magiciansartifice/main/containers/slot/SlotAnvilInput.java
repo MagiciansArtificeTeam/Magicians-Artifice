e93fb7dd4d32927dd56e367e5c4c59b876e787a5
James Mills <ThePugPug89@gmail.com>
2014-10-04 18:00:10 +1000

    New anvil gui texture.

---
+++ src/main/java/magiciansartifice/main/containers/slot/SlotAnvilInput.java
@@ -0,0 +1,34 @@
+package magiciansartifice.main.containers.slot;
+
+import magiciansartifice.main.items.ItemRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+import java.util.ArrayList;
+
+/**
+ * Created by poppypoppop on 4/10/2014.
+ */
+public class SlotAnvilInput extends Slot {
+    public static ArrayList<Block> blockInput = new ArrayList<Block>();
+    public static ArrayList<Item> itemInput = new ArrayList<Item>();
+
+    public SlotAnvilInput(IInventory inventory, int number, int xPos, int yPos) {
+        super(inventory, number, xPos, yPos);
+        itemInput.add(new ItemStack(ItemRegistry.ingotsMeta, 1, 1).getItem());
+        itemInput.add(new ItemStack(ItemRegistry.dustsMeta, 1, 0).getItem());
+    }
+
+    public boolean isItemValid(ItemStack par1ItemStack) {
+        for (Item item : itemInput) {
+            if (item == par1ItemStack.getItem()) return true;
+        }
+        for (Block block : blockInput) {
+            if (Item.getItemFromBlock(block) == par1ItemStack.getItem()) return true;
+        }
+        return false;
+    }
+}
