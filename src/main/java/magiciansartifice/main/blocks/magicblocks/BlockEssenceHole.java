381c4d9872eb76fe536f0aa1ee36949864b0fef2
James Mills <ThePugPug89@gmail.com>
2014-10-06 14:02:12 +1100

    Made the block walk throughable

--- src/main/java/magiciansartifice/main/blocks/magicblocks/BlockEssenceHole.java
+++ src/main/java/magiciansartifice/main/blocks/magicblocks/BlockEssenceHole.java
@@ -7,16 +7,21 @@ import magiciansartifice.main.items.magicalitems.ItemWand;
 import magiciansartifice.main.tileentities.magic.TileEntityEssenceHole;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 public class BlockEssenceHole extends BlockContainer {
 
     public BlockEssenceHole() {
         super(Material.rock);
-        this.setResistance(2000.0F);
         this.setHardness(5.0F);
+        this.setBlockUnbreakable();
+        this.setResistance(2000.0F);
         this.setBlockName("essenceHole");
         this.setCreativeTab(MagiciansArtifice.tab);
         this.setBlockTextureName(ModInfo.MODID + ":magicalblocks/essenceHole");
@@ -24,22 +29,18 @@ public class BlockEssenceHole extends BlockContainer {
         BlockRegistry.blocks.add(this);
     }
 
-    @Override
-    public boolean isOpaqueCube() {
-        return false;
-    }
+    public void addCollisionBoxesToList(World world, int x, int y, int z, AxisAlignedBB aabb, List list, Entity entity) {}
 
-    @Override
+    public boolean isOpaqueCube() { return false; }
+
     public boolean renderAsNormalBlock() {
         return false;
     }
 
-    @Override
     public TileEntity createNewTileEntity(World world, int meta) {
         return new TileEntityEssenceHole();
     }
 
-    @Override
     public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
         if (player.getCurrentEquippedItem() != null) {
             if (player.getCurrentEquippedItem().getItem() instanceof ItemWand) {
