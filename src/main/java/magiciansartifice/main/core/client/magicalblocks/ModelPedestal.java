f41e2a4942e0b90f316a5691937572900324a2b4
James Mills <ThePugPug89@gmail.com>
2014-12-06 21:41:24 +1100

    Fixed the model and did more work on the random structure

--- src/main/java/magiciansartifice/main/core/client/magicalblocks/ModelPedestal.java
+++ src/main/java/magiciansartifice/main/core/client/magicalblocks/ModelPedestal.java
@@ -4,62 +4,54 @@ import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 
-public class ModelPedestal extends ModelBase
-{
-    //fields
-    ModelRenderer Base;
-    ModelRenderer Connector;
-    ModelRenderer ConnectorTop;
-    ModelRenderer Top;
-  
-    public ModelPedestal()
-    {
-        textureWidth = 128;
-        textureHeight = 64;
-    
-        Base = new ModelRenderer(this, 0, 0);
-        Base.addBox(0F, 0F, 0F, 14, 3, 14);
-        Base.setRotationPoint(-7F, 21F, -7F);
-        Base.setTextureSize(128, 64);
-        Base.mirror = true;
-        setRotation(Base, 0F, 0F, 0F);
-        Connector = new ModelRenderer(this, 41, 18);
-        Connector.addBox(0F, 0F, 0F, 8, 10, 8);
-        Connector.setRotationPoint(-4F, 11F, -4F);
-        Connector.setTextureSize(128, 64);
-        Connector.mirror = true;
-        setRotation(Connector, 0F, 0F, 0F);
-        ConnectorTop = new ModelRenderer(this, 0, 19);
-        ConnectorTop.addBox(0F, 0F, 0F, 10, 1, 10);
-        ConnectorTop.setRotationPoint(-5F, 11F, -5F);
-        ConnectorTop.setTextureSize(128, 64);
-        ConnectorTop.mirror = true;
-        setRotation(ConnectorTop, 0F, 0F, 0F);
-        Top = new ModelRenderer(this, 57, 0);
-        Top.addBox(0F, 0F, 0F, 12, 3, 12);
-        Top.setRotationPoint(-6F, 8F, -6F);
-        Top.setTextureSize(128, 64);
-        Top.mirror = true;
-        setRotation(Top, 0F, 0F, 0F);
+public class ModelPedestal extends ModelBase {
+    public ModelRenderer Connector;
+    public ModelRenderer Top;
+    public ModelRenderer ConnectorTop;
+    public ModelRenderer Base;
+    public ModelRenderer BaseStair1;
+    public ModelRenderer BaseStair2;
+
+    public ModelPedestal() {
+        this.textureWidth = 128;
+        this.textureHeight = 64;
+        this.Top = new ModelRenderer(this, 57, 0);
+        this.Top.setRotationPoint(-6.0F, 8.0F, -6.0F);
+        this.Top.addBox(0.0F, 0.0F, 0.0F, 12, 3, 12);
+        this.Base = new ModelRenderer(this, 0, 0);
+        this.Base.setRotationPoint(-7.0F, 21.0F, -7.0F);
+        this.Base.addBox(0.0F, 0.0F, 0.0F, 14, 3, 14);
+        this.BaseStair2 = new ModelRenderer(this, 0, 32);
+        this.BaseStair2.setRotationPoint(-6.0F, 20.0F, -6.0F);
+        this.BaseStair2.addBox(0.0F, 0.0F, 0.0F, 12, 1, 12);
+        this.Connector = new ModelRenderer(this, 41, 18);
+        this.Connector.setRotationPoint(-4.0F, 11.0F, -4.0F);
+        this.Connector.addBox(0.0F, 0.0F, 0.0F, 8, 10, 8);
+        this.ConnectorTop = new ModelRenderer(this, 0, 19);
+        this.ConnectorTop.setRotationPoint(-5.0F, 11.0F, -5.0F);
+        this.ConnectorTop.addBox(0.0F, 0.0F, 0.0F, 10, 1, 10);
+        this.BaseStair1 = new ModelRenderer(this, 0, 19);
+        this.BaseStair1.setRotationPoint(-5.0F, 19.0F, -5.0F);
+        this.BaseStair1.addBox(0.0F, 0.0F, 0.0F, 10, 1, 10);
     }
-  
-    public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
-    {
-        super.render(entity, f, f1, f2, f3, f4, f5);
-        setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-        Base.render(f5);
-        Connector.render(f5);
-        ConnectorTop.render(f5);
-        Top.render(f5);
+
+    @Override
+    public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+        this.Top.render(f5);
+        this.Base.render(f5);
+        this.BaseStair2.render(f5);
+        this.Connector.render(f5);
+        this.ConnectorTop.render(f5);
+        this.BaseStair1.render(f5);
     }
-  
+
     private void setRotation(ModelRenderer model, float x, float y, float z)
     {
         model.rotateAngleX = x;
         model.rotateAngleY = y;
         model.rotateAngleZ = z;
     }
-  
+
     public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity)
     {
         super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
@@ -71,5 +63,7 @@ public class ModelPedestal extends ModelBase
         this.Connector.render(0.625F);
         this.ConnectorTop.render(0.625F);
         this.Top.render(0.625F);
+        this.BaseStair1.render(0.625F);
+        this.BaseStair2.render(0.625F);
     }
 }
--- src/main/java/magiciansartifice/main/items/Debugger.java
+++ src/main/java/magiciansartifice/main/items/Debugger.java
@@ -12,16 +12,41 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Debugger extends Item {
+    Random rand = new Random();
+    public static ArrayList<Item> solidBlocks = new ArrayList<Item>();
+    public static ArrayList<Item> nonSolidBlocks = new ArrayList<Item>();
+
 	public Debugger() {
 		super();
 		this.setCreativeTab(MagiciansArtifice.tab);
 		this.setUnlocalizedName("debugger");
 		this.setTextureName(ModInfo.MODID + ":debugger");
 		ItemRegistry.items.add(this);
+        assignItems();
 	}
-	
-	@Override
+
+    private void assignItems() {
+        for (int i = 0; i <= 3; i++) {
+            solidBlocks.add(new ItemStack(Blocks.stonebrick, 1, i).getItem());
+        }
+        solidBlocks.add(Item.getItemFromBlock(Blocks.cobblestone));
+        solidBlocks.add(Item.getItemFromBlock(Blocks.gravel));
+        solidBlocks.add(Item.getItemFromBlock(Blocks.mossy_cobblestone));
+        nonSolidBlocks.add(new ItemStack(Blocks.stone_slab, 1, 3).getItem());
+        nonSolidBlocks.add(new ItemStack(Blocks.stone_slab, 1, 5).getItem());
+        for (int i = 0; i <= 3; i++) {
+            nonSolidBlocks.add(new ItemStack(Blocks.stone_brick_stairs, 1, i).getItem());
+        }
+        for (int i = 0; i <= 3; i++) {
+            nonSolidBlocks.add(new ItemStack(Blocks.stone_stairs, 1, i).getItem());
+        }
+    }
+
+    @Override
 	public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
         if (player.isSneaking()) reverseEffect(world, x, y, z);
         else doEffect(world, x, y, z);
@@ -29,35 +54,15 @@ public class Debugger extends Item {
     }
 
     private void doEffect(World world, int x, int y, int z) {
-        Block block = Blocks.stonebrick;
-        int x1 = x;
-        int y1 = y;
-        int z1 = z - 6;
-
-        world.setBlock(x1, y1, z1, block);
-        z1 += 1;
-        x1 -= 2;
-        for (int i = 0; i <= 4; i++) {
-            world.setBlock(x1 + i, y1, z1, block);
-        }
-        z1 += 1;
-        x1 -= 2;
-        for (int i = 0; i <= 8; i++) {
-            world.setBlock(x1 + i, y1, z1, block);
-        }
-        z1 += 1;
-        for (int i = 0; i <= 8; i++) {
-            world.setBlock(x1 + i, y1, z1, block);
-        }
-        z1 += 1;
-        x1 -= 1;
-        for (int i = 0; i <= 10; i++) {
-            world.setBlock(x1 + i, y1, z1, block);
-        }
+        spawnBase(world, x, y, z, Blocks.stonebrick);
+        crumbleBase(world, x, y, z);
     }
 
     private void reverseEffect(World world, int x, int y, int z) {
-        Block block = Blocks.grass;
+        spawnBase(world, x, y, z, Blocks.grass);
+    }
+
+    private void spawnBase(World world, int x, int y, int z, Block block) {
         int x1 = x;
         int y1 = y;
         int z1 = z - 6;
@@ -82,5 +87,73 @@ public class Debugger extends Item {
         for (int i = 0; i <= 10; i++) {
             world.setBlock(x1 + i, y1, z1, block);
         }
+        z1 += 1;
+        for (int i = 0; i <= 10; i++) {
+            world.setBlock(x1 + i, y1, z1, block);
+        }
+        z1 += 1;
+        x1 -= 1;
+        for (int i = 0; i <= 12; i++) {
+            world.setBlock(x1 + i, y1, z1, block);
+        }
+        z1 += 1;
+        x1 += 1;
+        for (int i = 0; i <= 10; i++) {
+            world.setBlock(x1 + i, y1, z1, block);
+        }
+        z1 += 1;
+        for (int i = 0; i <= 10; i++) {
+            world.setBlock(x1 + i, y1, z1, block);
+        }
+        z1 += 1;
+        x1 += 1;
+        for (int i = 0; i <= 8; i++) {
+            world.setBlock(x1 + i, y1, z1, block);
+        }
+        z1 += 1;
+        for (int i = 0; i <= 8; i++) {
+            world.setBlock(x1 + i, y1, z1, block);
+        }
+        z1 += 1;
+        x1 += 2;
+        for (int i = 0; i <= 4; i++) {
+            world.setBlock(x1 + i, y1, z1, block);
+        }
+        z1 += 1;
+        x1 += 2;
+        world.setBlock(x1, y1, z1, block);
+    }
+
+    private void crumbleBase(World world, int x, int y, int z) {
+        int x1 = x - 6;
+        int z1 = z - 6;
+        for (int i = 0; i < 13; i++) {
+            for (int ii = 0; i < 13; i++) {
+                if (!(world.getBlock(x, y, z) == Blocks.stonebrick)) {
+                    boolean doSolid = rand.nextBoolean();
+                    if (!doSolid) {
+                        doSolid = rand.nextBoolean();
+                        if (!doSolid) {
+                            doSolid = rand.nextBoolean();
+                        }
+                    }
+                    int chance = rand.nextInt(100);
+                    if (chance >= 80) {
+                        if (doSolid) doSolid(world, x1 + ii, y, z1 + i);
+                        else doNonSolid(world, x1 + ii, y, z1 + i);
+                    }
+                }
+            }
+        }
+    }
+
+    private void doNonSolid(World world, int x, int y, int z) {
+        int random = rand.nextInt(nonSolidBlocks.size() + 1);
+        world.setBlock(x, y, z, Block.getBlockFromItem(nonSolidBlocks.get(random)));
+    }
+
+    private void doSolid(World world, int x, int y, int z) {
+        int random = rand.nextInt(solidBlocks.size() + 1);
+        world.setBlock(x, y, z, Block.getBlockFromItem(solidBlocks.get(random)));
     }
 }
